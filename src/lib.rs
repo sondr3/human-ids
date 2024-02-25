7c4ec0a4c398f07cd9af7d7b8e151e366e8a39c4
Sondre Aasemoen <sondre@eons.io>
2024-02-25 16:53:38 +0100

    Implement human-id

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,134 @@
-pub mod cli;
+pub mod constants;
+
+pub use constants::{ADJECTIVES, ADVERBS, NOUNS, VERBS};
+
+#[must_use]
+pub fn random<'a>(vec: &'a [&'a str]) -> &'a str {
+    let index = fastrand::usize(..vec.len());
+    vec[index]
+}
+
+#[must_use]
+pub fn longest<'a>(vec: &'a [&'a str]) -> &'a str {
+    vec.iter().max_by_key(|s| s.len()).unwrap_or(&"")
+}
+
+#[must_use]
+pub fn shortest<'a>(vec: &'a [&'a str]) -> &'a str {
+    vec.iter().min_by_key(|s| s.len()).unwrap_or(&"")
+}
+
+fn capitalize(s: &str) -> String {
+    let mut chars = s.chars();
+    chars.next().map_or_else(String::new, |c| {
+        c.to_uppercase().collect::<String>() + chars.as_str()
+    })
+}
+
+#[derive(Debug)]
+pub struct Options<'a> {
+    pub separator: Option<&'a str>,
+    pub capitalize: bool,
+    pub add_adverb: bool,
+    pub adjective_count: usize,
+}
+
+impl Default for Options<'_> {
+    fn default() -> Self {
+        Self {
+            separator: None,
+            capitalize: true,
+            add_adverb: false,
+            adjective_count: 1,
+        }
+    }
+}
+
+impl Options<'_> {
+    #[must_use]
+    pub fn builder<'a>() -> OptionsBuilder<'a> {
+        OptionsBuilder::default()
+    }
+}
+
+#[derive(Debug)]
+pub struct OptionsBuilder<'a> {
+    separator: Option<&'a str>,
+    capitalize: bool,
+    add_adverb: bool,
+    adjective_count: usize,
+}
+
+impl Default for OptionsBuilder<'_> {
+    fn default() -> Self {
+        Self {
+            separator: None,
+            capitalize: true,
+            add_adverb: false,
+            adjective_count: 1,
+        }
+    }
+}
+
+impl<'a> OptionsBuilder<'a> {
+    #[must_use]
+    pub const fn separator(mut self, separator: &'a str) -> Self {
+        self.separator = Some(separator);
+        self
+    }
+
+    #[must_use]
+    pub const fn capitalize(mut self, capitalize: bool) -> Self {
+        self.capitalize = capitalize;
+        self
+    }
+
+    #[must_use]
+    pub const fn add_adverb(mut self, add_adverb: bool) -> Self {
+        self.add_adverb = add_adverb;
+        self
+    }
+
+    #[must_use]
+    pub const fn adjective_count(mut self, adjective_count: usize) -> Self {
+        self.adjective_count = adjective_count;
+        self
+    }
+
+    #[must_use]
+    pub const fn build(self) -> Options<'a> {
+        Options {
+            separator: self.separator,
+            capitalize: self.capitalize,
+            add_adverb: self.add_adverb,
+            adjective_count: self.adjective_count,
+        }
+    }
+}
+
+pub fn human_id(options: Option<Options>) -> String {
+    let options = options.unwrap_or_default();
+
+    let mut words = Vec::new();
+
+    for _ in 0..options.adjective_count {
+        words.push(random(&ADJECTIVES));
+    }
+
+    words.push(random(&NOUNS));
+    words.push(random(&VERBS));
+
+    if options.add_adverb {
+        words.push(random(&ADVERBS));
+    }
+
+    let words: Vec<_> = if options.capitalize {
+        words.iter().map(|s| capitalize(s)).collect()
+    } else {
+        words.into_iter().map(String::from).collect()
+    };
+
+    options
+        .separator
+        .map_or_else(|| words.join(""), |sep| words.join(sep))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
-use anyhow::Result;
-use clap::CommandFactory;
-use human_ids::cli::{self, Cli};
+use std::io;
 
-fn main() -> Result<()> {
-    let cli = Cli::init();
+use clap::{Command, CommandFactory, Parser};
+use clap_complete::{generate, Generator, Shell};
+use human_ids::{human_id, Options};
+
+#[derive(Parser, Debug)]
+#[command(name = "changeset", about, version, author)]
+pub struct Cli {
+    /// Generate shell completion scripts
+    ///
+    /// Generates shell completion scripts for the specified shell.
+    #[clap(long, value_enum)]
+    pub completion: Option<Shell>,
+
+    /// The separator to use between words
+    #[clap(long, short, default_value = "-")]
+    pub separator: String,
+
+    /// Capitalize each word
+    #[clap(long, short)]
+    pub capitalize: bool,
+
+    /// Add an adverb
+    #[clap(long, short, default_value = "false")]
+    pub adverb: bool,
+
+    /// The number of adjectives to use
+    #[clap(long, short, default_value = "1")]
+    pub num_adjectives: usize,
+}
+
+pub fn print_completion<G: Generator>(gen: G, app: &mut Command) {
+    generate(gen, app, app.get_name().to_string(), &mut io::stdout());
+}
+
+fn main() {
+    let cli = Cli::parse();
 
     if let Some(shell) = cli.completion {
-        cli::print_completion(shell, &mut Cli::command());
-        return Ok(());
+        print_completion(shell, &mut Cli::command());
+        return;
     }
 
-    Ok(())
+    let options = Options::builder()
+        .separator(&cli.separator)
+        .capitalize(cli.capitalize)
+        .add_adverb(cli.adverb)
+        .adjective_count(cli.num_adjectives)
+        .build();
+
+    println!("{}", human_id(Some(options)));
 }
