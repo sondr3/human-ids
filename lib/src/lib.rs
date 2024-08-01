b8be8904e289f5718293ca47b9f29b82f897d5b0
Cain <75994858+cainthebest@users.noreply.github.com>
2024-08-01 14:25:28 +0100

    refactor(lib): structure a bit better and clean up

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -2,89 +2,94 @@
 pub mod constants;
 
 pub use constants::{ADJECTIVES, ADVERBS, NOUNS, VERBS};
+use std::fmt;
 
-#[must_use]
-/// Returns a random element from the given vector
-///
-/// ## Example
-/// ```
-/// # use human_ids::random;
-/// let vec = vec!["a", "b", "c"];
-/// assert!(vec.contains(&random(&vec)));
-/// ```
-pub fn random<'a>(vec: &'a [&'a str]) -> &'a str {
-    let index = fastrand::usize(..vec.len());
-    vec[index]
+/// Trait for selecting elements from a collection.
+pub trait Selector<'a> {
+    /// Returns a random element from the collection.
+    ///
+    /// ## Example
+    /// ```
+    /// # use human_ids::Selector;
+    /// let vec = vec!["a", "b", "c"];
+    /// assert!(vec.contains(&vec.random()));
+    /// ```
+    fn random(&self) -> &'a str;
+
+    /// Returns the longest element from the collection.
+    ///
+    /// ## Example
+    /// ```
+    /// # use human_ids::Selector;
+    /// let vec = vec!["a", "ab", "abc"];
+    /// assert_eq!(vec.longest(), "abc");
+    /// ```
+    fn longest(&self) -> &'a str;
+
+    /// Returns the shortest element from the collection.
+    ///
+    /// ## Example
+    /// ```
+    /// # use human_ids::Selector;
+    /// let vec = vec!["a", "ab", "abc"];
+    /// assert_eq!(vec.shortest(), "a");
+    /// ```
+    fn shortest(&self) -> &'a str;
 }
 
-#[must_use]
-/// Returns the longest element from the given vector
-///
-/// ## Example
-/// ```
-/// # use human_ids::longest;
-/// let vec = vec!["a", "ab", "abc"];
-/// assert_eq!(longest(&vec), "abc");
-/// ```
-pub fn longest<'a>(vec: &'a [&'a str]) -> &'a str {
-    vec.iter().max_by_key(|s| s.len()).unwrap_or(&"")
+impl<'a> Selector<'a> for [&'a str] {
+    #[inline]
+    fn random(&self) -> &'a str {
+        let index = fastrand::usize(..self.len());
+        self[index]
+    }
+
+    fn longest(&self) -> &'a str {
+        self.iter().max_by_key(|s| s.len()).unwrap_or(&"")
+    }
+
+    fn shortest(&self) -> &'a str {
+        self.iter().min_by_key(|s| s.len()).unwrap_or(&"")
+    }
 }
 
-#[must_use]
-/// Returns the shortest element from the given vector
-///
-/// ## Example
-/// ```
-/// # use human_ids::shortest;
-/// let vec = vec!["a", "ab", "abc"];
-/// assert_eq!(shortest(&vec), "a");
-/// ```
-pub fn shortest<'a>(vec: &'a [&'a str]) -> &'a str {
-    vec.iter().min_by_key(|s| s.len()).unwrap_or(&"")
+/// Trait for capitalizing the first letter of a string.
+pub trait Capitalize {
+    /// Capitalizes the first letter of the string.
+    ///
+    /// ## Example
+    /// ```
+    /// # use human_ids::Capitalize;
+    /// let s = "hello";
+    /// assert_eq!(s.capitalize(), "Hello");
+    /// ```
+    fn capitalize(&self) -> String;
 }
 
-fn capitalize(s: &str) -> String {
-    let mut chars = s.chars();
-    chars.next().map_or_else(String::new, |c| {
-        c.to_uppercase().collect::<String>() + chars.as_str()
-    })
+impl Capitalize for &str {
+    #[inline]
+    fn capitalize(&self) -> String {
+        let mut chars = self.chars();
+        chars.next().map_or_else(String::new, |c| {
+            c.to_uppercase().collect::<String>() + chars.as_str()
+        })
+    }
 }
 
 #[derive(Debug)]
+/// Options for generating a human-readable identifier.
 pub struct Options<'a> {
+    /// Separator to use between words.
     pub separator: Option<&'a str>,
+    /// Whether to capitalize each word.
     pub capitalize: bool,
+    /// Whether to add an adverb to the end of the identifier.
     pub add_adverb: bool,
+    /// Number of adjectives to use.
     pub adjective_count: usize,
 }
 
 impl Default for Options<'_> {
-    fn default() -> Self {
-        Self {
-            separator: None,
-            capitalize: true,
-            add_adverb: false,
-            adjective_count: 1,
-        }
-    }
-}
-
-impl Options<'_> {
-    #[must_use]
-    pub fn builder<'a>() -> OptionsBuilder<'a> {
-        OptionsBuilder::default()
-    }
-}
-
-#[derive(Debug)]
-pub struct OptionsBuilder<'a> {
-    separator: Option<&'a str>,
-    capitalize: bool,
-    add_adverb: bool,
-    adjective_count: usize,
-}
-
-impl Default for OptionsBuilder<'_> {
     fn default() -> Self {
         Self {
             separator: Some("-"),
@@ -95,106 +100,188 @@ impl Default for OptionsBuilder<'_> {
     }
 }
 
-impl<'a> OptionsBuilder<'a> {
-    #[must_use]
-    /// Set the separator to use between words
+impl<'a> Options<'a> {
+    /// Creates a new instance of `Options`.
     ///
     /// ## Example
     /// ```
     /// # use human_ids::Options;
+    /// let options = Options::new();
+    /// ```
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Sets the separator to use between words.
     ///
-    /// let options = Options::builder()
-    ///     .separator("-")
-    ///     .build();
+    /// ## Example
+    /// ```
+    /// # use human_ids::Options;
+    /// let options = Options::new().separator("-");
     /// ```
     pub const fn separator(mut self, separator: &'a str) -> Self {
         self.separator = Some(separator);
         self
     }
 
-    #[must_use]
-    /// Set whether to capitalize each word
+    /// Sets whether to capitalize each word.
     ///
     /// ## Example
     /// ```
     /// # use human_ids::Options;
-    ///
-    /// let options = Options::builder()
-    ///     .capitalize(true)
-    ///     .build();
+    /// let options = Options::new().capitalize(true);
     /// ```
     pub const fn capitalize(mut self, capitalize: bool) -> Self {
         self.capitalize = capitalize;
         self
     }
 
-    #[must_use]
-    /// Set whether to add an adverb to the end of the identifier
+    /// Sets whether to add an adverb to the end of the identifier.
     ///
     /// ## Example
     /// ```
     /// # use human_ids::Options;
-    ///
-    /// let options = Options::builder()
-    ///     .add_adverb(true)
-    ///     .build();
+    /// let options = Options::new().add_adverb(true);
     /// ```
     pub const fn add_adverb(mut self, add_adverb: bool) -> Self {
         self.add_adverb = add_adverb;
         self
     }
 
-    #[must_use]
-    /// Set the number of adjectives to use
+    /// Sets the number of adjectives to use.
     ///
     /// ## Example
     /// ```
     /// # use human_ids::Options;
-    ///
-    /// let options = Options::builder()
-    ///     .adjective_count(2)
-    ///     .build();
+    /// let options = Options::new().adjective_count(2);
     /// ```
     pub const fn adjective_count(mut self, adjective_count: usize) -> Self {
         self.adjective_count = adjective_count;
         self
     }
+}
 
-    #[must_use]
-    pub const fn build(self) -> Options<'a> {
-        Options {
-            separator: self.separator,
-            capitalize: self.capitalize,
-            add_adverb: self.add_adverb,
-            adjective_count: self.adjective_count,
+#[derive(Debug)]
+/// Structure for generating human-readable identifiers.
+pub struct HumanId<'a> {
+    options: Options<'a>,
+}
+
+impl<'a> HumanId<'a> {
+    /// Creates a new `HumanId` instance with the given options.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use human_ids::{HumanId, Options};
+    /// let options = Options::new();
+    /// let human_id = HumanId::new(Some(options));
+    /// ```
+    pub fn new(options: Option<Options<'a>>) -> Self {
+        Self {
+            options: options.unwrap_or_default(),
         }
+    }
+
+    /// Generates a human-readable identifier based on the options.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// # use human_ids::{HumanId, Options};
+    /// let options = Options::new();
+    /// let human_id = HumanId::new(Some(options));
+    /// let id = human_id.generate();
+    /// ```
+    pub fn generate(&self) -> String {
+        let options = &self.options;
+
+        let mut words = Vec::with_capacity(
+            options.adjective_count + 2 + if options.add_adverb { 1 } else { 0 },
+        );
+
+        for _ in 0..options.adjective_count {
+            words.push(ADJECTIVES.random());
+        }
+
+        words.push(NOUNS.random());
+        words.push(VERBS.random());
+
+        if options.add_adverb {
+            words.push(ADVERBS.random());
+        }
+
+        let words: Vec<_> = if options.capitalize {
+            words.iter().map(|s| s.capitalize()).collect()
+        } else {
+            words.into_iter().map(String::from).collect()
+        };
+
+        options
+            .separator
+            .map_or_else(|| words.join("-"), |sep| words.join(sep))
     }
 }
 
-/// Generate a human-readable identifier
-pub fn generate(options: Option<Options>) -> String {
-    let options = options.unwrap_or_default();
+impl fmt::Display for HumanId<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.generate())
+    }
+}
 
-    let mut words = Vec::new();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    for _ in 0..options.adjective_count {
-        words.push(random(&ADJECTIVES));
+    #[test]
+    fn test_random() {
+        let vec = vec!["a", "b", "c"];
+        assert!(vec.contains(&vec.random()));
     }
 
-    words.push(random(&NOUNS));
-    words.push(random(&VERBS));
-
-    if options.add_adverb {
-        words.push(random(&ADVERBS));
+    #[test]
+    fn test_longest() {
+        let vec = vec!["a", "ab", "abc"];
+        assert_eq!(vec.longest(), "abc");
     }
 
-    let words: Vec<_> = if options.capitalize {
-        words.iter().map(|s| capitalize(s)).collect()
-    } else {
-        words.into_iter().map(String::from).collect()
-    };
+    #[test]
+    fn test_shortest() {
+        let vec = vec!["a", "ab", "abc"];
+        assert_eq!(vec.shortest(), "a");
+    }
 
-    options
-        .separator
-        .map_or_else(|| words.join("-"), |sep| words.join(sep))
+    #[test]
+    fn test_capitalize() {
+        let s = "hello";
+        assert_eq!(s.capitalize(), "Hello");
+    }
+
+    #[test]
+    fn test_generate() {
+        let options = Options::new()
+            .separator("_")
+            .capitalize(true)
+            .add_adverb(true)
+            .adjective_count(2);
+
+        let id = HumanId::new(Some(options)).generate();
+
+        println!("{}", id);
+        assert!(!id.is_empty());
+    }
+
+    #[test]
+    fn test_display() {
+        let options = Options::new()
+            .separator("_")
+            .capitalize(true)
+            .add_adverb(true)
+            .adjective_count(2);
+
+        let id = HumanId::new(Some(options)).to_string();
+
+        println!("{}", id);
+        assert!(!id.is_empty());
+    }
 }
